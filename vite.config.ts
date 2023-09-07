2544c627dcdc0dd73355cafa2e92c0f86fd631a0
Jacob Allen Valderama <vjacoballen3040@gmail.com>
2023-09-07 11:11:04 +0800

    fixing the deployment test 2

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,13 @@ import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
-export default defineConfig({ plugins: [react(), tsconfigPaths()], })
+export default defineConfig({
+    plugins: [react(), tsconfigPaths()],
+    build: {
+        sourcemap: true,
+        commonjsOptions: {
+            esmExternals: true
+        },
+
+    },
+})
