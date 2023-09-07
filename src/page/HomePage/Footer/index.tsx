8c8736f2439ba8bc6af66bc58bbf8e67f286ed21
Jacob Allen Valderama <vjacoballen3040@gmail.com>
2023-09-07 17:16:53 +0800

    :update

--- src/page/HomePage/Footer/index.tsx
+++ src/page/HomePage/Footer/index.tsx
@@ -4,8 +4,8 @@ export const Footer = () => {
 	return (
 		<div className=' flex flex-col items-center'>
 
-			<div className='text-[1.8rem] p-4 text-center'>
-                @2023 Porfolio Created by <span className="font-DotGothic  text-primary-orange">Jacob Allen Valderama</span>  
+			<div className='text-[1.5rem] p-4 text-center'>
+                @2023 Porfolio Created by <span className="font-poppins  text-primary-orange">Jacob Allen Valderama</span>  
 			</div>
 		</div>
 	)
