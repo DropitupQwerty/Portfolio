f6f849ed95ada8cab67117bbf21ca6527cd2ad20
DropitUpQwery <vjacoballen2040@gmail.com>
2023-09-08 15:23:18 +0800

    fully update portfolio

--- src/page/HomePage/About/About.tsx
+++ src/page/HomePage/About/About.tsx
@@ -34,7 +34,7 @@ export const About = () => {
 						<div className='font-poppins text-justify w-[80%] lg:w-[40%]'>
 							<div className='text-6xl tracking-widest font-[600] '><span className="text-primary-orange">WHO AM I?</span> </div>
 							<div className='indent-10 leading-relaxed text-3xl lg:text-4xl  dark:text-primary-white'>
-								<p className='mb-10'>Hello! I am a fresh graduate student that aspires to be a skilled web developer , capable of creating user-friendly online apps, proficient in <span className='text-primary-orange'>Java, JavaScript, TypeScript </span> and informed about popular frameworks such as React . outstanding debugging and issue-solving abilities, a keen eye for detail, and a passion for producing high-caliber code.
+								<p className='mb-10'>Hello! I am a fresh graduate student that aspires to be a skilled web developer, capable of creating user-friendly online apps, proficient in <span className='text-primary-orange'>Java, JavaScript, TypeScript </span> and informed about popular frameworks such as React. outstanding debugging and issue-solving abilities, a keen eye for detail, and a passion for producing high-caliber code.
 								</p>
 								
 							</div>
