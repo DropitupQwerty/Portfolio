983ba79f7388a962424b294826f550d41bf9dab7
DropitUpQwery <vjacoballen2040@gmail.com>
2023-09-12 10:17:24 +0800

    stop selecting text

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { HomePage } from 'page/HomePage'
 function App() {
 	return (
-		<div className='dark:text-primary-white text-primary-black dark:bg-black/80 duration-300 ' >
+		<div className='dark:text-primary-white text-primary-black select-none dark:bg-black/80 duration-300 ' >
 			<HomePage/>		
 		</div>
 	)
