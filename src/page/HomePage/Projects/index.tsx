c15fa61d12dd4c1d2b25d3afb489c2dbab2e5238
Jacob allen <vjacoballen3040@gmail.com>
2025-04-29 04:06:05 +0800

    Rephrase recent project to Recent projects and collaborations

--- src/page/HomePage/Projects/index.tsx
+++ src/page/HomePage/Projects/index.tsx
@@ -10,7 +10,7 @@ export const Projects = () => {
 		<div id="projects" className=' dark:bg-transparent '>
 			<div className=' '>
 				<div className='container gap-20  mx-auto flex items-center justify-center  '>
-					<div className='text-5xl lg:text-7xl font-[600] mb-[5%] font-poppins whitespace-nowrap text-primary-orange'>MY RECENT WORKS</div>
+					<div className='text-5xl lg:text-7xl font-[600] mb-[5%] font-poppins whitespace-nowrap text-primary-orange'>RECENT PROJECTS & COLLABORATIONS</div>
 				</div>
 
 				<div className=' flex flex-col gap-20   duration-300  relative '>
